adb7de2f0ef86b3bbe4c717cb937e4d2c431b492
lhicks94 <logan@legacysafetymanagement.com>
2026-04-22 08:50:44 -0500

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,20 @@
-
+const chemicals = [
+  {
+    name: "Brakleen Brake Parts Cleaner",
+    signalWord: "Danger",
+    pictograms: ["flame", "gas-cylinder", "exclamation"],
+    sdsUrl: "https://www.crcindustries.com/media/msdsen/msds_en-1008009.pdf"
+  },
+  {
+    name: "Simple Green Industrial Cleaner",
+    signalWord: "None",
+    pictograms: [],
+    sdsUrl: "https://cdn.simplegreen.com/downloads/SDS_EN-US_SimpleGreenIndustrialCleanerDegreaser.pdf"
+  },
+  {
+    name: "Ultratane Butane Fuel",
+    signalWord: "Danger",
+    pictograms: ["flame", "gas-cylinder"],
+    sdsUrl: "https://www.masterappliance.com/content/sites/default/files/simple_products/downloads/2024-SDS-Ultratane-Butane-English.pdf"
+  }
+];
